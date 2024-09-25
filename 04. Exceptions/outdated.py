73e361f647dd74aa3a32d64939f18e065381850c
Diogo <diogofrmota@gmail.com>
2024-09-25 11:55:39 +0100

    Correction for February month

--- 04. Exceptions/outdated.py
+++ 04. Exceptions/outdated.py
@@ -1,3 +1,5 @@
+import calendar
+
 months = [
     "January",
     "February",
@@ -13,29 +15,13 @@ months = [
     "December",
 ]
 
-
 while True:
     date = input("Date: ")
     if len(date.split("/")) == 3:
         date = date.split("/")
         try:
             month, day, year = map(int, date)
-            if 1 <= month <= 12 and 1 <= day <= 31:
-                print(f"{year:04d}{month:02d}{day:02d}")
-                break
-        except ValueError:
-            pass
-
-
-
-
-while True:
-    date = input("Date: ")
-    if len(date.split("/")) == 3:
-        date = date.split("/")
-        try:
-            month, day, year = map(int, date)
-            if 1 <= month <= 12 and 1 <= day <= 31:
+            if 1 <= month <= 12 and 1 <= day <= calendar.monthrange(year, month)[1]:
                 print(f"{year:04d}-{month:02d}-{day:02d}")
                 break
         except ValueError:
@@ -47,8 +33,4 @@ while True:
                 month, day = date[0].split()
                 month = months.index(month) + 1
                 day, year = int(day), int(date[1])
-                if 1 <= month <= 12 and 1 <= day <= 31:
-                    print(f"{year:04d}-{month:02d}-{day:02d}")
-                    break
-            except ValueError:
-                pass
+                if 1 <= month <= 12 and 1 <= day <= calendar.monthrange(year, month)[1]:
