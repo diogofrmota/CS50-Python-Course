7b0b977e6cbc6da69a38fbc21114f34c2f606029
Diogo <diogofrmota@gmail.com>
2024-09-26 17:01:30 +0100

    Added more tests

--- 06. Unit Tests/test_bank/test_bank.py
+++ 06. Unit Tests/test_bank/test_bank.py
@@ -1,10 +1,15 @@
-from bank import value
+from bank import get_value
 
-
-def test_value():
-    assert value("Hello") == 0
-    assert value(" Hello ") == 0
-    assert value("Hello, Newman") == 0
-    assert value("How you doing?") == 20
-    assert value("What's happening?") == 100
-    assert value("What's up?") == 100
+def test_get_value():
+    assert get_value("Hello") == 0
+    assert get_value(" Hello ") == 0
+    assert get_value("Hello, Newman") == 0
+    assert get_value("how you doing?") == 20
+    assert get_value("What's happening?") == 100
+    assert get_value("What's up?") == 100
+    assert get_value("HELLO") == 0
+    assert get_value("") == 100
+    assert get_value(" H") == 20
+    assert get_value("h") == 20
+    assert get_value("hELLO") == 0
+    assert get_value("  HELLO  ") == 0
