741f7d93c01211c7529fac7b7e946b227f97154e
Diogo <diogofrmota@gmail.com>
2024-09-27 08:26:28 +0100

    Removed infinite loop

--- 06. Unit Tests/test_fuel/fuel.py
+++ 06. Unit Tests/test_fuel/fuel.py
@@ -1,9 +1,9 @@
 def main():
-    print(gauge(convert(input("Fraction: "))))
+    print(gauge(convert(get_fraction())))
 
-
-def convert(fraction):
+def get_fraction():
     while True:
+        fraction = input("Fraction: ")
         try:
             x, y = map(int, fraction.split("/"))
             if y == 0:
@@ -12,17 +12,15 @@ def convert(fraction):
                 raise ValueError
             return round(x / y * 100)
         except (ValueError, ZeroDivisionError):
-            pass
-
+            print("Invalid fraction. Please input a proper fraction (numerator < denominator) and denominator should not be zero.")
 
 def gauge(percentage):
-    if percentage <= 1:
+    if percentage <= 0:
         return "E"
     elif percentage >= 99:
         return "F"
     else:
         return f"{percentage}%"
-
 
 if __name__ == "__main__":
     main()
